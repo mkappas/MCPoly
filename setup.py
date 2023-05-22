4bd841fdcec28e5a65491b9794e7523d91f39316
Omicron-Fluor <115920350+Omicron-Fluor@users.noreply.github.com>
2023-05-22 10:35:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='MCPoly',
-    version='0.3.0',
+    version='0.3.1',
     description='Useful tools for Computational Chemistry for polymers',
     long_description_content_type="text/markdown",
     long_description=README,
